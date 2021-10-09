7bd54c314efe6c691e769530cf080c500975b75c
sj <ischos02@gmila.com>
2021-10-09 15:42:03 +0900

    버그 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -11,20 +11,24 @@ const convertedCountry = document.querySelector("#converted-country");
 //국가 정보 변경
 const countryChange = async (e) => {
     await countryChangeApi(e.target.value);
-    fn_submitBtn();
+    price.value ? fn_submitBtn() : hide();
     countryCode.innerHTML=` ${e.target.value}/USD`;
 }
 
 const fn_submitBtn = () => {
     const m = fn_calculation();
     if(m === 0) {
-        price.value = "";
-        main.classList.remove("show");
+        hide();
         return false;
     }
-    convertedAmount.innerHTML = m;
+    convertedAmount.innerHTML = numberWithCommas(m);
     convertedCountry.innerHTML = country.selectedOptions[0].value;
     main.classList.add("show");
+}
+
+const hide = () => {
+    price.value = "";
+    main.classList.remove("show");
 }
 
 //환율 정보 호출
@@ -40,7 +44,7 @@ const countryChangeApi = (value) => {
             }
             return json;
         })
-        .then(json => exchangeRateAmount.innerHTML = roundToTwo(json.money))
+        .then(json => exchangeRateAmount.innerHTML = numberWithCommas(roundToTwo(json.money)))
         .catch(err => console.log(err));
 }
 
@@ -57,12 +61,19 @@ const fn_calculation = () => {
     if(_exchangeRate === "0" || !_exchangeRate) {
         return 0;
     }
-    return roundToTwo(parseFloat(_price) * parseFloat(_exchangeRate)).toFixed(2);
+    return roundToTwo(parseFloat(_price.replace(",", ""))
+                        * parseFloat(_exchangeRate.replace(",", "")))
+                    .toFixed(2);
 }
 
 //반올림 함수
 const roundToTwo = (num) => {
     return +(Math.round(num + "e+2")  + "e-2");
+}
+
+//자리수 콤마
+const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 country.addEventListener("change", countryChange);
